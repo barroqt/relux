530c009c67b0a71190c11cd9009648f03d661255
barroqt <barroqt@gmail.com>
2024-08-29 13:09:11 +0200

    add certfication from authorized partners

--- backend/contracts/ReluxMarketplace.sol
+++ backend/contracts/ReluxMarketplace.sol
@@ -10,25 +10,48 @@ contract ReluxMarketplace is ERC721, Ownable, ReentrancyGuard {
     IERC20 public usdcToken;
     uint256 public listingCount;
 
+    // TODO: Implement this enum instead of booleans for status
+    // enum ListingStatus {
+    //     Active,
+    //     Sold,
+    //     Disputed
+    // }
+
     struct Listing {
-        uint256 watchId;
+        uint256 productId;
         address payable seller;
         uint256 price;
         uint256 listingTime;
         bool isDisputed;
         bool isSold;
+        bool isCertified;
+        address partner;
     }
 
     mapping(uint256 => Listing) public listings;
+    mapping(address => bool) public authorizedPartners;
+    mapping(uint256 => address) public certifiedProductsByPartners;
+    mapping(uint256 => uint256) public productIdToTokenId;
+
     uint256 public constant DISPUTE_PERIOD = 3 days;
     uint256 public constant PLATFORM_FEE_PERCENT = 2; // 2% platform fee
 
-    event WatchListed(uint256 indexed tokenId, uint256 indexed watchId, address seller, uint256 price);
-    event WatchSold(uint256 indexed tokenId, uint256 indexed watchId, address buyer, uint256 price);
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event ProductListed(uint256 indexed tokenId, uint256 indexed productId, address seller, uint256 price);
+    event ProductSold(uint256 indexed tokenId, uint256 indexed productId, address buyer, uint256 price);
     event ListingDisputed(uint256 indexed tokenId, address disputer);
     event ListingCancelled(uint256 indexed tokenId);
+    event PartnerAuthorized(address partner);
+    event PartnerDeauthorized(address partner);
+    event ProductCertified(uint256 productId, address partner);
+    event ProductCertificationRevoked(uint256 productId, address partner);
+    event ListingRemoved(uint256 indexed tokenId, uint256 indexed productId);
 
-    // Custom errors
+    /*//////////////////////////////////////////////////////////////
+                             CUSTOM ERRORS
+    //////////////////////////////////////////////////////////////*/
     error ProductAlreadySold();
     error ListingInDisputedState();
     error DisputePeriodNotOver();
@@ -40,7 +63,14 @@ contract ReluxMarketplace is ERC721, Ownable, ReentrancyGuard {
     error OnlyOwnerCanCancel();
     error CannotTransferSoldListing();
     error CannotTransferDisputedListing();
+    error UnauthorizedPartner();
+    error ProductAlreadyCertified();
+    error ProductNotCertified();
+    error UnauthorizedCaller();
 
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
     modifier notSold(uint256 tokenId) {
         if (listings[tokenId].isSold) revert ProductAlreadySold();
         _;
@@ -58,12 +88,29 @@ contract ReluxMarketplace is ERC721, Ownable, ReentrancyGuard {
         _;
     }
 
+    modifier onlyAuthorizedPartner() {
+        if (!authorizedPartners[msg.sender]) revert UnauthorizedPartner();
+        _;
+    }
+
+    modifier onlyOwnerOrAuthorizedPartner() {
+        if (msg.sender != owner() && !authorizedPartners[msg.sender]) revert UnauthorizedCaller();
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
     constructor(address _usdcToken) ERC721("ReluxWatchListing", "RWL") {
         usdcToken = IERC20(_usdcToken);
     }
 
-    function createListing(uint256 watchId, uint256 price) external {
-        if (price <= 0) revert PriceMustBeGreaterThanZero();
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function createListing(uint256 _productId, uint256 _price) external {
+        if (_price <= 0) revert PriceMustBeGreaterThanZero();
+        if (certifiedProductsByPartners[_productId] == address(0)) revert ProductNotCertified(); // TODO: test this
 
         listingCount++;
         uint256 newTokenId = listingCount;
@@ -71,26 +118,34 @@ contract ReluxMarketplace is ERC721, Ownable, ReentrancyGuard {
         _safeMint(msg.sender, newTokenId);
 
         listings[newTokenId] = Listing({
-            watchId: watchId,
+            productId: _productId,
             seller: payable(msg.sender),
-            price: price,
+            price: _price,
             listingTime: block.timestamp,
             isDisputed: false,
-            isSold: false
+            isSold: false,
+            isCertified: true,
+            partner: certifiedProductsByPartners[_productId]
         });
 
-        emit WatchListed(newTokenId, watchId, msg.sender, price);
+        productIdToTokenId[_productId] = newTokenId;
+
+        emit ProductListed(newTokenId, _productId, msg.sender, _price);
     }
 
-    function buyWatch(uint256 tokenId)
+    function buyProduct(uint256 _tokenId)
         external
         nonReentrant
-        notSold(tokenId)
-        notDisputed(tokenId)
-        disputePeriodOver(tokenId)
+        notSold(_tokenId)
+        notDisputed(_tokenId)
+        disputePeriodOver(_tokenId)
     {
-        if (!_exists(tokenId)) revert ListingDoesNotExist();
-        Listing storage listing = listings[tokenId];
+        if (!_exists(_tokenId)) revert ListingDoesNotExist();
+
+        Listing storage listing = listings[_tokenId];
+
+        if (listing.isSold) revert ProductAlreadySold();
+        if (listing.isDisputed) revert ListingInDisputedState();
 
         uint256 platformFee = (listing.price * PLATFORM_FEE_PERCENT) / 100;
         uint256 sellerAmount = listing.price - platformFee;
@@ -100,47 +155,87 @@ contract ReluxMarketplace is ERC721, Ownable, ReentrancyGuard {
         if (!usdcToken.transfer(owner(), platformFee)) revert PlatformFeeTransferFailed();
 
         listing.isSold = true;
-        _transfer(listing.seller, msg.sender, tokenId);
+        _transfer(listing.seller, msg.sender, _tokenId);
 
-        emit WatchSold(tokenId, listing.watchId, msg.sender, listing.price);
+        emit ProductSold(_tokenId, listing.productId, msg.sender, listing.price);
     }
 
-    function disputeListing(uint256 tokenId)
+    function disputeListing(uint256 _tokenId)
         external
-        notSold(tokenId)
-        notDisputed(tokenId)
-        disputePeriodOver(tokenId)
+        notSold(_tokenId)
+        notDisputed(_tokenId)
+        disputePeriodOver(_tokenId)
     {
-        if (!_exists(tokenId)) revert ListingDoesNotExist();
-        Listing storage listing = listings[tokenId];
+        if (!_exists(_tokenId)) revert ListingDoesNotExist();
+        Listing storage listing = listings[_tokenId];
 
         listing.isDisputed = true;
 
-        emit ListingDisputed(tokenId, msg.sender);
+        emit ListingDisputed(_tokenId, msg.sender);
     }
 
-    function cancelListing(uint256 tokenId) external notSold(tokenId) notDisputed(tokenId) {
-        if (!_exists(tokenId)) revert ListingDoesNotExist();
-        if (ownerOf(tokenId) != msg.sender) revert OnlyOwnerCanCancel();
+    function cancelListing(uint256 _tokenId) external notSold(_tokenId) notDisputed(_tokenId) {
+        if (!_exists(_tokenId)) revert ListingDoesNotExist();
+        if (ownerOf(_tokenId) != msg.sender) revert OnlyOwnerCanCancel();
 
-        delete listings[tokenId];
-        _burn(tokenId);
+        delete listings[_tokenId];
+        _burn(_tokenId);
 
-        emit ListingCancelled(tokenId);
+        emit ListingCancelled(_tokenId);
     }
 
-    function getListing(uint256 tokenId) external view returns (Listing memory) {
-        if (!_exists(tokenId)) revert ListingDoesNotExist();
-        return listings[tokenId];
+    function updateUSDCToken(address _newUSDCToken) external onlyOwner {
+        usdcToken = IERC20(_newUSDCToken);
     }
 
-    function updateUSDCToken(address newUSDCToken) external onlyOwner {
-        usdcToken = IERC20(newUSDCToken);
+    function authorizePartner(address _partner) external onlyOwner {
+        authorizedPartners[_partner] = true;
+        emit PartnerAuthorized(_partner);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-        if (listings[tokenId].isSold) revert CannotTransferSoldListing();
-        if (listings[tokenId].isDisputed) revert CannotTransferDisputedListing();
+    function deauthorizePartner(address _partner) external onlyOwner {
+        authorizedPartners[_partner] = false;
+        emit PartnerDeauthorized(_partner);
+    }
+
+    function certifyProduct(uint256 _productId) external onlyAuthorizedPartner {
+        if (certifiedProductsByPartners[_productId] != address(0)) revert ProductAlreadyCertified();
+        certifiedProductsByPartners[_productId] = msg.sender;
+        emit ProductCertified(_productId, msg.sender);
+    }
+
+    // TODO: test this
+    function revokeCertification(uint256 _productId) external onlyOwnerOrAuthorizedPartner {
+        address partner = certifiedProductsByPartners[_productId];
+        if (partner == address(0)) revert ProductNotCertified();
+
+        delete certifiedProductsByPartners[_productId];
+
+        uint256 tokenId = productIdToTokenId[_productId];
+        if (tokenId != 0 && _exists(tokenId)) {
+            delete listings[tokenId];
+            _burn(tokenId);
+            delete productIdToTokenId[_productId];
+            emit ListingRemoved(tokenId, _productId);
+        }
+
+        emit ProductCertificationRevoked(_productId, partner);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function _beforeTokenTransfer(address _from, address _to, uint256 _tokenId, uint256 _batchSize) internal override {
+        super._beforeTokenTransfer(_from, _to, _tokenId, _batchSize);
+        if (listings[_tokenId].isSold) revert CannotTransferSoldListing();
+        if (listings[_tokenId].isDisputed) revert CannotTransferDisputedListing();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+    function getListing(uint256 _tokenId) external view returns (Listing memory) {
+        if (!_exists(_tokenId)) revert ListingDoesNotExist();
+        return listings[_tokenId];
     }
 }
